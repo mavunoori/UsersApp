6b3ab7ed7823a8a7b263fbbeca476affbcae27c3
avunoma <33365065+avunoma@users.noreply.github.com>
2020-06-04 19:42:57 +0530

    Add project files.

---
+++ UsersApp/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using UsersApp.Model;
+
+namespace UsersApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext applicationDbContext;
+
+        public IndexModel(ApplicationDbContext applicationDbContext)
+        {
+            this.applicationDbContext = applicationDbContext;
+        }
+
+        public IList<User> Users;
+        public void OnGet()
+        {
+            Users =applicationDbContext.Users.ToList();
+        }
+    }
+}
